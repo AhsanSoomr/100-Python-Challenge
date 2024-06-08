4eb6a28a917e205b08740205ff405a951dc95246
Ahsan Ali <69108718+AhsanSoomr@users.noreply.github.com>
2024-06-08 10:49:25 +0000

    question 1

---
+++ 1_Swap-Two-Variables.py
@@ -0,0 +1,17 @@
+print("Question 1 : Swap Two Variables")
+
+# assign a variable 
+x = 5
+y = 10
+
+# here is we create a temporary variable and swap the values
+temp = x
+x = y
+y = temp
+
+print('The value of x after swapping:',x)
+print('The value of y after swapping:',y)
+
+
+
+
